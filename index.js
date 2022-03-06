79f484905902e54d9457cfb202b4d8de19c35762
bimi <bimihiddg@gmail.com>
2022-03-06 23:22:58 +0330

    style & js

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ const checkbox = document.getElementById("checkbox");
 checkbox.addEventListener("click" , ()=>{
     document.body.classList.toggle("dark")
 })
+
+const write = document.getElementById("write");
+let rar = "World's Biggest univercity"
+let idx = 1;
+setInterval(tipe , 120);
+function tipe(){
+write.innerText = rar.slice(0,idx);
+idx++;
+}
+
+const lev = document.getElementById("score-lev");
+setInterval(() => {
+    lev.style.width = "82%";
+    lev.style.opacity = 1
+}, 1000);
+
